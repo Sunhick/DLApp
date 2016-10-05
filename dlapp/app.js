606067ed14ab26a7153e083b6d6f1436af56f24a
Sunil <sunhick@gmail.com>
2016-10-05 17:33:17 -0600

    migrated to html

--- dlapp/app.js
+++ dlapp/app.js
@@ -11,8 +11,16 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views/'));
-app.set('view engine', 'pug');
+var cons = require('consolidate');
+
+// view engine setup
+app.engine('html', cons.swig)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+
+//app.set('views', path.join(__dirname, 'views/'));
+//app.set('view engine', require('ejs').renderFile);
+//app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
