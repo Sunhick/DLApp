dc1dd7ff866b8f67eb5663767748993b40f605ed
Sunil <sunhick@gmail.com>
2016-10-06 16:07:43 -0600

    added complete schema for the projects

--- dlapp/models/projects.js
+++ dlapp/models/projects.js
@@ -8,7 +8,41 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var projectSchema = new Schema({
-    name: String
+    // primary faculty information
+    fName: String,
+    phoneNumber: String,
+    fEmail: String,
+    fDepartment: String,
+    fCommunities: Boolean,
+
+    // secondary faculty information
+    secName: String,
+    secphoneNumber: String,
+    secEmail: String,
+    secDepartment: String,
+    secCommunities: Boolean,
+
+    // grad student information
+    gradName: String,
+    gradPhoneNumber: String,
+    gradEmail: String,
+
+    // apprenticeship information
+    title: String,
+    weblink: String,
+    specialReq: String,
+    desc: String,
+    areas: [String],
+    supervisionAmt: String,
+    supervisionBy: String,
+    natureWrk: String,
+    priorWrk: String,
+
+    // finances
+    finances: String,
+
+    //other
+    pastApprentice: Boolean
 });
 
 var Project = mongoose.model('Project', projectSchema);
