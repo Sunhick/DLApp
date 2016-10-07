849f155fd5c7830c69ff1020d402616f390d905d
kepai91 <keerthipai91@gmail.com>
2016-10-06 18:52:44 -0600

    Added the student schema in project.js and updated package.json to add mongoose-file

--- dlapp/models/projects.js
+++ dlapp/models/projects.js
@@ -5,12 +5,78 @@
 *  Copyright (c) 2016. University of Colorado, boulder
 */
 var mongoose = require('mongoose');
+var filePluginLib = require('mongoose-file');
+var filePlugin = filePluginLib.filePlugin;
+var make_upload_to_model = filePluginLib.make_upload_to_model;
+
 var Schema = mongoose.Schema;
+
+var uploads_base = path.join(__dirname, "uploads");
+var uploads = path.join(uploads_base, "u");
 
 var projectSchema = new Schema({
     name: String
 });
 
+var studentSchema = new Schema({
+    firstName: String
+    lastName: String
+    gender: String
+    origin: String
+    race: String
+    addressLine1Boulder: String
+    addressLine2Boulder: String
+    cityBoulder: String
+    stateBoulder: String
+    zipBoulder: Number
+    emailBoulder: String
+    phoneNumerBouler: String
+    addressLine1Summer: String
+    addressLine2Summer: String
+    citySummer: String
+    stateSummer: String
+    zipSummer: String
+    emailSummer: String
+    phoneNumberSummer: String
+    primaryMajor: String
+    gpa: Number
+    secondaryMajor: String
+    schoolLevel: String
+    listGradMonth: String
+    listGradYear: Number
+    sid: Number
+    researchExperience: String
+    appliedBefore: String
+    otherFallEmployment: String 
+    projectInterest: String
+    firstChoice: String
+    secondChoice: String
+    thirdChoice: String
+    fourthChoice: String
+    fifthChoice: String
+    backgroundCheck: String
+    backgroundCheckSem: String
+    backgroundCheckYear: String
+    awarenessTraining: String
+    awarenessTrainingSem: String
+    awarenessTrainingYear: String
+    ssnInfo: Number
+    firstSkill: String
+    secondSkill: String
+    thirdSkill: String
+ });
+
+studentSchema.plugin(filePlugin, {
+   name: "resume",
+   upload_to: make_upload_to_model(uploads, 'resume')
+});
+
+studentSchema.plugin(filePlugin, {
+   name: "coverLetter",
+   upload_to: make_upload_to_model(uploads, 'coverLetter')
+});
+       
 var Project = mongoose.model('Project', projectSchema);
+var Student = mongoose.model('Student', studentSchema);
 
 module.exports = Project;
