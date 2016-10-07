3fc6c414691ea1675e7608b719d7a08796f3d359
Sunil <sunhick@gmail.com>
2016-10-06 19:59:58 -0600

    moved student schema to student file

--- dlapp/models/students.js
+++ dlapp/models/students.js
@@ -8,6 +8,64 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var studentSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    gender: String,
+    origin: String,
+    race: String,
+    addressLine1Boulder: String,
+    addressLine2Boulder: String,
+    cityBoulder: String,
+    stateBoulder: String,
+    zipBoulder: Number,
+    emailBoulder: String,
+    phoneNumerBouler: String,
+    addressLine1Summer: String,
+    addressLine2Summer: String,
+    citySummer: String,
+    stateSummer: String,
+    zipSummer: String,
+    emailSummer: String,
+    phoneNumberSummer: String,
+    primaryMajor: String,
+    gpa: Number,
+    secondaryMajor: String,
+    schoolLevel: String,
+    listGradMonth: String,
+    listGradYear: Number,
+    sid: Number,
+    researchExperience: String,
+    appliedBefore: String,
+    otherFallEmployment: String,
+    projectInterest: String,
+    firstChoice: String,
+    secondChoice: String,
+    thirdChoice: String,
+    fourthChoice: String,
+    fifthChoice: String,
+    backgroundCheck: String,
+    backgroundCheckSem: String,
+    backgroundCheckYear: String,
+    awarenessTraining: String,
+    awarenessTrainingSem: String,
+    awarenessTrainingYear: String,
+    ssnInfo: Number,
+    firstSkill: String,
+    secondSkill: String,
+    thirdSkill: String,
+ });
+
+studentSchema.plugin(filePlugin, {
+   name: "resume",
+   upload_to: make_upload_to_model(uploads, 'resume')
+});
+
+studentSchema.plugin(filePlugin, {
+   name: "coverLetter",
+   upload_to: make_upload_to_model(uploads, 'coverLetter')
+});
+
+var studentSchema = new Schema({
     // primary faculty information
     firstName: String,
     lastName: String
