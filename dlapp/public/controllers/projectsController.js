cb6a893f7db7a0b1606b344dbcc3c17b8fac5609
Sunil <sunhick@gmail.com>
2016-10-07 13:52:41 -0600

    pull available projects from the backend

--- dlapp/public/controllers/projectsController.js
+++ dlapp/public/controllers/projectsController.js
@@ -6,19 +6,24 @@
 */
 angular.module('dlapp').controller('projectsController', function($http){
     var self = this;
-    self.projects = [
-        { name : "project-22" },
-        { name : "Level it" }
-    ];
+
+    // Get the list of projects from the backend(db)
+    // Todo: if the list is too big, try local caching(angular local storage or http cache?)
+    self.getProjectsList = function() {
+        var config = { cache: false };
+        $http.get("/projects/list", config).then(function(response) {
+            self.projects = response.data;
+        });
+    }();
 
     self.submitProject = function(project) {
         // create the data
-        var data = { name: project.name };
-
+        var data = { title: project.title };
+        console.log(project.title);
         // post the data
         $http.post("/projects/add", data)
             .then(function(response) {
-                self.projects.push(data);
+                self.projects.push(response.data);
             });
     }
 });
--- dlapp/routes/projects.js
+++ dlapp/routes/projects.js
@@ -5,20 +5,18 @@
 *  Copyright (c) 2016. University of Colorado, boulder
 */
 
-var mongoose = require('mongoose');
-var Project = require('../models/projects')
-var dbConfig = require('../config/db')
+var mongoose = require("mongoose");
+var Project = require("../models/projects")
+var dbConfig = require("../config/db")
 
 mongoose.connect(dbConfig.mongo.uri);
 
 module.exports = function(router) {
     // add project 
-    router.post('/projects/add', function(req, res, next) {
-        console.log('got request!' + req.body.name);
+    router.post("/projects/add", function(req, res, next) {
+        console.log("got request!" + req.body.name);
         
-        var prj = new Project({
-            name : req.body.name
-        });
+        var prj = new Project({ title: req.body.title });
 
         prj.save(function(err) {
             if(err) {
@@ -31,8 +29,23 @@ module.exports = function(router) {
     });
 
     // projects page
-    router.get('/projects', function(req, res, next) {
-        res.render('partials/projects');
+    router.get("/projects", function(req, res, next) {
+        res.render("partials/projects");
+    });
+
+    router.get("/projects/list", function(req, res, next) {
+        var projects = [];
+        
+        // query mongo db for the list of registered projects
+        Project.find({}, function(err, records) {
+            records.forEach(function(record) {
+                var project = {};
+                project.title = record.title;
+                projects.push(project);
+            });
+
+            res.send(projects);
+        });
     });
     // module.exports = router;
 }
