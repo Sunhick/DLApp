66585772f143b1e9f8a3784b6a7a809577f83e14
Pallaviclear <pama1314@colorado.edu>
2016-10-06 22:35:50 -0600

    Fixed merging errors

--- dlapp/public/controllers/studentController.js
+++ dlapp/public/controllers/studentController.js
@@ -7,12 +7,9 @@
 angular.module('dlapp').controller('studentController', function($http){
     var self = this;
     self.data = {};
-<<<<<<< HEAD
 
     var Register = $resource('/students/register');
-=======
->>>>>>> f519eb41f6b6150af226b8510c8b1d85f5159f3d
-
+    
     self.submit = function(data) {
         console.log(data);
         $http.post('/students/register', data);
