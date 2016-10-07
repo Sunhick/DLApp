6a9bcfde06c5fa16c61e76d7d5b8eb0d36cb10ed
Sunil <Sunhick@gmail.com>
2016-10-07 17:37:05 -0600

    Create logging.js

---
+++ dlapp/config/logging.js
@@ -0,0 +1,9 @@
+/**
+*  @Author: Sunil bn <sunhick@gmail.com>
+*  @Desc: configuration for logging in backend
+*
+*  Copyright (c) 2016. University of Colorado, boulder
+*/
+module.exports = {
+    // to be added later.
+};
