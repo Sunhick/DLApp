2dbf61ce9c1d9873c6fca5a132f845a13c34144c
surajsridhar <such2660@colorado.edu>
2016-11-04 22:30:53 -0600

    Updated the checkboxes and radio button data

--- dlapp/public/controllers/facultyController.js
+++ dlapp/public/controllers/facultyController.js
@@ -1,16 +1,27 @@
 /**
 *  @Author: Sunil bn <sunhick@gmail.com>
-*  @Desc: faculty controller 
+*  @Desc: faculty controller
 *
 *  Copyright (c) 2016. University of Colorado, boulder
 */
-angular.module('dlapp').controller('facultyController', ['$http', '$location',
- function($http, $location) {
+angular.module('dlapp').controller('facultyController', ['$http', '$scope', '$location',
+ function($http, $scope, $location) {
     "use strict";
-    
+
     var self = this;
 
     self.submit = function(project) {
+        var areas = []
+        for (var key in project.areas) {
+          if (project.areas.hasOwnProperty(key)) {
+            areas.push(key);
+          }
+        }
+        project.areas = areas;
+        if(project.natureWrk == "others")
+          project.natureWrk = $scope.natureWrk;
+        if(project.priorWrk == "others")
+          project.priorWrk = $scope.priorWrk
         console.log(project);
         // post the data
         $http.post("/projects/add", project)
