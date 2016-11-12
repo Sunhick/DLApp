a56c8aa27f0a52820c9d63fb82d2336e11591790
Sunil <sunhick@gmail.com>
2016-11-12 00:10:03 -0700

    added save button

--- dlapp/public/controllers/matchController.js
+++ dlapp/public/controllers/matchController.js
@@ -11,36 +11,20 @@ angular.module('dlapp').controller('matchController', ['$http', function($http) 
     // dummy data, to be fetched from backend
     self.matches = [
         {
-            studentName: "John",
-            project1: "project-a-1",
-            project2: "project-a-2",
-            project3: "project-a-3",
-            project4: "project-a-4",
-            project5: "project-a-5",
+            studentName: "John cart",
+            assigned: "Project-1"
         },
         {
-            studentName: "Lucy",
-            project1: "project-b-1",
-            project2: "project-b-2",
-            project3: "project-b-3",
-            project4: "project-b-4",
-            project5: "project-b-5",
+            studentName: "Lucy meyers",
+            assigned: "Project-4"
         },
         {
-            studentName: "William",
-            project1: "project-c-1",
-            project2: "project-c-2",
-            project3: "project-c-3",
-            project4: "project-c-4",
-            project5: "project-c-5",
+            studentName: "William Luck",
+            assigned: "Project-3"
         },
         {
-            studentName: "Sunny",
-            project1: "project-d-1",
-            project2: "project-d-2",
-            project3: "project-d-3",
-            project4: "project-d-4",
-            project5: "project-d-5",
+            studentName: "Sunil Murthy",
+            assigned: "Project-2"
         },
     ];
 
@@ -53,5 +37,6 @@ angular.module('dlapp').controller('matchController', ['$http', function($http) 
 
     self.setSelection = function(student) {
         self.selectedStudent = student;
+        self.tempAssigned = self.selectedStudent.assigned;
     };
 }]);
