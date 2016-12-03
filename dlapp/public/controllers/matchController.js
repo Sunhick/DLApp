dae1b9a04188ff33b80f2313a50c1b72ce72484a
Pallavi Madasu <pallavimadasu@Pallavis-MacBook-Pro.local>
2016-12-02 23:14:44 -0700

    Changes to auto-match projects after manual override

--- dlapp/public/controllers/matchController.js
+++ dlapp/public/controllers/matchController.js
@@ -10,6 +10,7 @@ angular.module('dlapp').controller('matchController', ['$http', function($http) 
 
     // Get the list of projects from database
     self.projects=[];
+
     self.getProjectsList = function() {
         $http.get("/projects/list").then(function(response) {
             self.projects = response.data;
@@ -18,6 +19,14 @@ angular.module('dlapp').controller('matchController', ['$http', function($http) 
 
     // dummy data, to be fetched from backend
     self.matches = [];
+
+    // Reset count of students assigned to each project, everytime the page is loaded
+    self.resetCountsOfAssignedStudents = function(){
+      console.log('***** Reset Counts *****');
+        for (var i=0; i < self.projects.length; i++){
+          self.projects[i].updatedCount = 0;
+      }
+    };
 
     self.getStudentPreferences = function(studentName) {
       var prefs = [];
@@ -48,25 +57,62 @@ angular.module('dlapp').controller('matchController', ['$http', function($http) 
     };
 
 
-
     // Get the list of students from database and auto-match project for each student
     self.students=[];
     self.getStudentsList = function() {
+        self.resetCountsOfAssignedStudents();
         $http.get("/students/list").then(function(response) {
             self.students = response.data;
+            var autoMatchedStudents = [];
+            var j = 0;
             for(var i=0; i < self.students.length; i++){
                 var student = self.students[i];
-                console.log('Student name - '+student.firstName);
-                var projectAssigned = self.getAutoMatch(student);
                 var studentName = student.firstName.concat('  ' ,student.lastName);
-                self.matches[i]={'studentName':studentName, 'assigned':projectAssigned};
+                // console.log('Student name - '+studentName);
 
-                // Save the project and student mapping
-                self.saveAssignedProjectAndStudent(student,projectAssigned);
+                // Update manual student-project assignment     
+                if(self.manuallyOverriddenStudents.indexOf(studentName) > -1){
+                  var mAssignedProject = self.studentProjectMap[studentName];
+                  // console.log('Manually Assigned Project : '+mAssignedProject);
+                  
+                   var countOfStudentsAssigned = self.getCountOfStudentsAssignedForProject(mAssignedProject);
+                    if(countOfStudentsAssigned == undefined){
+                      countOfStudentsAssigned = 0;
+                    }
+                    
+                    var maxStudentsForProject = self.getMaxStudentsRequiredForProject(mAssignedProject);
+                
+                    // If the assigned students count is less than reuired students count
+                    if(countOfStudentsAssigned < maxStudentsForProject){
+                        
+                        // Update count of student-project assignment (manual override)
+                        self.matches[j++]={'studentName':studentName, 'assigned': mAssignedProject};
+                        self.saveUpdatedCount(mAssignedProject,countOfStudentsAssigned+1);
+                    } else {
+                      //console.log('Required limit reached...!!!');
+                      var projectAssigned = self.getAutoMatch(student);                
+                      self.matches[j++]={'studentName':studentName, 'assigned':projectAssigned};
+                    }
+                } else 
+                {
+                  autoMatchedStudents.push(student);
+                }
             }
-            console.log("Final List", self.matches);
+            
+            //console.log('**** autoMatchedStudents : '+autoMatchedStudents);
+
+            for(var i = 0; i < autoMatchedStudents.length; i++ ){
+                var student = autoMatchedStudents[i];
+                var studentName = student.firstName.concat('  ' ,student.lastName);
+
+                // Call auto-match script to get the system assigned project          
+                var projectAssigned = self.getAutoMatch(student);                
+                self.matches[j++]={'studentName':studentName, 'assigned':projectAssigned};
+            }
+            //console.log("Final List", self.matches);
         });
-    }();
+    };
+    self.getStudentsList();
 
     // Check the eligibility of student for a given project and return true/false
     self.checkEligibilityOfStudent = function(student, project){
@@ -113,11 +159,6 @@ angular.module('dlapp').controller('matchController', ['$http', function($http) 
       }
     };
 
-    // Save the project assigned to student in database
-    self.saveAssignedProjectAndStudent = function(student,project){
-        // Write code
-    };
-
     // Auto match student and project
     self.getAutoMatch = function(student) { 
          var projectPreferences = [student.firstChoice, student.secondChoice, student.thirdChoice, student.fourthChoice, student.fifthChoice];
@@ -126,19 +167,18 @@ angular.module('dlapp').controller('matchController', ['$http', function($http) 
 
             if (projectPreferences[i] != undefined){
               var project = projectPreferences[i];
+              
               // If student is eligible, assign the project
-              console.log("checking eligibility for", project);
+              
               if(self.checkEligibilityOfStudent(student, project)){
                 // Get the count of students already assigned for the project
                 var countOfStudentsAssigned = self.getCountOfStudentsAssignedForProject(project);
                 if(countOfStudentsAssigned == undefined){
                   countOfStudentsAssigned = 0;
                 }
-                //console.log('countOfStudentsAssigned - '+countOfStudentsAssigned);
-                
+  
                 // Get the count of required students for the project
                 var maxStudentsForProject = self.getMaxStudentsRequiredForProject(project);
-                //console.log('maxStudentsForProject - '+maxStudentsForProject);
                 
                 if(countOfStudentsAssigned < maxStudentsForProject){
                     // Update count of students assigned
@@ -155,6 +195,22 @@ angular.module('dlapp').controller('matchController', ['$http', function($http) 
     self.setSelection = function(student) {
         self.selectedStudent = student;
         self.tempAssigned = {"title": self.selectedStudent.assigned};
+    };
+
+    self.manuallyOverriddenStudents = [];
+    self.studentProjectMap = {};
+
+    self.autoMatchAfterManualOverride = function(studentName){
+        
+        self.selectedStudent.assigned = self.tempAssigned.title;
+
+        // Update manually overridden students list
+        self.manuallyOverriddenStudents.push(studentName);
+        // Save manually overridden student project assignment in a map
+        self.studentProjectMap[studentName] =  self.tempAssigned.title;
+
+        // Re-run auto-match script
+        self.getStudentsList();
     };
 
 }]);
